f2aeb9db986bf91c17fd17ef92462deb5cd1817b
jamesmilner20 <jamesduah20@gmail.com>
2017-09-30 09:39:17 +0000

    calculation of total and subtotal calculated

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,9 @@
-// function newNumber (number) {
-//   this.
-// }
-// function hold ($('#diceRole').val().split())
-
 function game() {
   this.total=0;
   this.subtotal=0;
   this.roll=0;
   this.turn=0;
 }
-
-// function switchPlayer() {
-//   if (this.roll===1) {
-//   }
-// }
 
 var newGame = new game();
 
@@ -29,8 +19,21 @@ game.prototype.additionOfSubtotal = function () {
     $('#output1').empty();
   }else if (this.roll !==1) {
       this.subtotal=this.subtotal+this.roll;
+      // this.total = this.total + this.subtotal;
   }
 };
+
+// function for the click of the hold button and alerting the winner
+game.prototype.totalNum = function () {
+  this.total += this.subtotal;
+  this.subtotal = 0;
+  $('#output1').empty();
+  if (this.bank>=100) {
+    alert ('You Won')
+  }
+};
+
+
 
 $(document).ready(function(){
   $("#roll").click(function (event) {
@@ -38,11 +41,13 @@ $(document).ready(function(){
       newGame.rollDice()
         $('#output1').append("<li>"+newGame.roll+"<li>");
         $('#ps1').text(newGame.subtotal);
-        $('p1').text(newGame.total);
 
   })
+  $("#hold").click(function (event) {
+      event.preventDefault()
+      newGame.totalNum()
+      $('#ps1').text(newGame.subtotal);
+      $('#p1').text(newGame.total);
+  })
 
-  // $("#hold").click(function(){
-
-  // })
 });
